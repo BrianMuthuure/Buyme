a0be81c842a01de120a63bbbe6290566a2c6df2d
Brian Muthuure <brianmuthuure@gmail.com>
2023-05-10 16:00:53 +0300

    chore:remove unnecessary spaces

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import PermissionsMixin
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
-
 from .managers import CustomUserManager
 from django.utils.translation import gettext_lazy as _
 
